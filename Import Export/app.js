0f126bf9325b371e87f3ab583d672c13caed1f74
Shavej-Malik <shavejsbi@gmail.com>
2026-09-15 08:35:11 +0530

    Import Export

---
+++ Import Export/app.js
@@ -0,0 +1,7 @@
+const user='Shavej Malik'
+export const username='Shavej_Malik' //Named Export
+export default user //This is default export
+
+export const math=98;
+export const phy=87;
+export const chem=89;
---
+++ Import Export/script.js
@@ -0,0 +1,5 @@
+import user from './app.js'
+import {username} from './app.js'
+import {math,phy,chem} from './app.js'
+console.log(math,phy,chem)
+console.log(user,username)
