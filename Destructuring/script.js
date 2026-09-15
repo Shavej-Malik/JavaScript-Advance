f620dacb53dd44ec3dfd9de8ac1c77e04441f248
Shavej-Malik <shavejsbi@gmail.com>
2026-09-15 08:34:04 +0530

    Destructuring

---
+++ Destructuring/script.js
@@ -0,0 +1,15 @@
+// var arr=[10,20,30,40]
+// var arr2=arr
+// arr2.push(50)
+// console.log(arr) //Output:-  [10, 20, 30, 40, 50]
+// console.log(arr2)//Output:-  [10, 20, 30, 40, 50]
+
+//Destruturing
+
+// var arr=[10,20,30,40]
+// var arr2=[...arr]
+// arr2.push(50)
+// console.log(arr)//Output:-  [10, 20, 30, 40]
+// console.log(arr2)//Output:-  [10, 20, 30, 40, 50]
+
+//same behave into object
